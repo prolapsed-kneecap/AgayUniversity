c58bc92e0baeb9f29865411e993b7f1b49e6d11b
ImPaha <56339635+ImPaha@users.noreply.github.com>
2021-02-28 18:54:26 +0700

    dddf

--- app/src/main/java/com/polich/kneecap/Event.kt
+++ app/src/main/java/com/polich/kneecap/Event.kt
@@ -1,3 +1,19 @@
 package com.polich.kneecap
 
-class Event(val eventText:String, val solutions:Array<Harvesters>)
+abstract class Event{
+    abstract val eventName:String
+    abstract val importance:Int
+    abstract val description:String
+    abstract fun damage(importance: Int): Int
+}
+class NaturalDisasters(override val eventName: String, override val importance: Int, override val description: String):Event(){
+    override fun damage(importance: Int): Int {
+        return 3
+    }
+
+}
+class Bugs(override val eventName: String, override val importance: Int, override val description: String):Event(){
+    override fun damage(importance: Int): Int {
+        return 3
+    }
+}
