e42626ced9c2e27ec4aab49010c828a81c9433cd
ImPaha <56339635+ImPaha@users.noreply.github.com>
2021-03-07 17:43:25 +0700

    backButton kruto

--- app/src/main/java/com/polich/kneecap/fragment/ResultFragment.kt
+++ app/src/main/java/com/polich/kneecap/fragment/ResultFragment.kt
@@ -33,12 +33,28 @@ import kotlinx.coroutines.launch
 var completedLevels = 0
 class ResultFragment : Fragment() {/*MainActivity.OnBackPressedListener*/
 
-    /*override fun onResume() {
-        super.onResume()
-
-        nextScreen()
-    }*/
-
+    override fun onDestroy() {
+        super.onDestroy()
+        val level = requireArguments().getString("levelNow")
+        if (level=="1"){
+            if (scoreHistory[0]<playerScore){
+                scoreHistory[0]= playerScore
+            }
+        }
+        if (level=="2"){
+            if (scoreHistory[1]<playerScore){
+                scoreHistory[1]= playerScore
+            }
+        }
+        if (level=="3"){
+            if (scoreHistory[2]<playerScore){
+                scoreHistory[2]= playerScore
+            }
+        }
+//            Toast.makeText(requireContext(), scoreHistory.toString(), LENGTH_SHORT).show()
+        completedLevels++
+        playerScore = 0
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -55,31 +71,12 @@ class ResultFragment : Fragment() {/*MainActivity.OnBackPressedListener*/
         star3.setColorFilter(Color.argb(255, 105, 105, 105))
 
         fillStars(star1, star2, star3, resultTextView)
-
         val level = requireArguments().getString("levelNow")
 
         //val callback = requireActivity().onBackPressedDispatcher.addCallback(this)
 
         go_to_selectLevel.setOnClickListener {
             Toast.makeText(requireContext(), "$level", LENGTH_SHORT).show()
-            if (level=="1"){
-                if (scoreHistory[0]<playerScore){
-                    scoreHistory[0]= playerScore
-                }
-            }
-            if (level=="2"){
-                if (scoreHistory[1]<playerScore){
-                    scoreHistory[1]= playerScore
-                }
-            }
-            if (level=="3"){
-                if (scoreHistory[2]<playerScore){
-                    scoreHistory[2]= playerScore
-                }
-            }
-//            Toast.makeText(requireContext(), scoreHistory.toString(), LENGTH_SHORT).show()
-            completedLevels++
-            playerScore = 0
             view.findNavController().navigate(R.id.action_resultFragment_to_levelSelectionFragment)
         }
 
@@ -87,10 +84,10 @@ class ResultFragment : Fragment() {/*MainActivity.OnBackPressedListener*/
     }
 
     fun fillStars(star1: ImageView, star2: ImageView, star3: ImageView, resultTextView: TextView){
-        if(playerScore<500){resultTextView.text = "Вы набрали "+(playerScore/10).toString()+" баллов из 100! \n Вам явно нужно больше практики"}
-        if (playerScore>=500){star1.setColorFilter(null); resultTextView.text = "Вы набрали "+(playerScore/10).toString()+" баллов из 100! \n Стоит потренироваться ещё!"/*; FilterStart(star1)*/}
-        if (playerScore>=750){star2.setColorFilter(null); resultTextView.text = "Вы набрали "+(playerScore/10).toString()+" баллов из 100! \n Вы молодец, но нужно больше практики."/*; FilterStart(star2)*/}
-        if (playerScore>=1000){star3.setColorFilter(null); resultTextView.text = "Вы набрали "+(1000/10).toString()+" баллов из 100! \n Идеально!"/*; FilterStart(star3)*/}
+        if(playerScore<300){resultTextView.text = "Вы набрали "+(playerScore/10).toString()+" баллов из 100! \n Вам явно нужно больше практики"}
+        if (playerScore>=300){star1.setColorFilter(null); resultTextView.text = "Вы набрали "+(playerScore/10).toString()+" баллов из 100! \n Стоит потренироваться ещё!"/*; FilterStart(star1)*/}
+        if (playerScore>=500){star2.setColorFilter(null); resultTextView.text = "Вы набрали "+(playerScore/10).toString()+" баллов из 100! \n Вы молодец, но нужно больше практики."/*; FilterStart(star2)*/}
+        if (playerScore>=900){star3.setColorFilter(null); resultTextView.text = "Вы набрали "+(1000/10).toString()+" баллов из 100! \n Идеально!"/*; FilterStart(star3)*/}
     }
     fun FilterStart(star:ImageView){
         MainScope().launch {
